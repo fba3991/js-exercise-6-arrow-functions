bc2da20468430dbe2aa0488dde0d208e5548b598
Fabioes <fabionike@icloud.com>
2023-11-07 00:12:54 +0100

    feat / primi 2 esercizi

---
+++ main.js
@@ -0,0 +1,23 @@
+/* const sottrazione = (num1, num2) => {
+    return num1 - num2;
+}
+const risultatoSottrazione = sottrazione(22, 20);
+console.log(risultatoSottrazione);
+ */
+
+/* const student ={
+    name: 'Kevin',
+    age: 16,
+    information: function(){
+        console.log(this.name, + this.age);
+    },
+    ageStudent: function(){
+        this.age++;
+    }
+     
+ };
+student.information();
+student.ageStudent();
+student.information();
+ */
+
